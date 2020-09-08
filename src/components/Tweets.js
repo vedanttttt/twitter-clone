bea63edf93927fdd49ec2b489b19ed98d6918d35
vedanttttt <51362126+vedanttttt@users.noreply.github.com>
2020-09-08 17:12:55 +0530

    added and modified sidebar component

---
+++ src/components/Tweets.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Tweets = () => {
+    return (
+        <div className="posts">
+            <div className="posts_home">
+                Home
+            </div>
+        </div>
+    )
+}
+
+export default Tweets
